a6bbbed38980d8214067145fdc280d0b57d059ac
AEverhub <asusgas097@gmail.com>
2025-04-20 17:36:31 +0800

    更新 maincode.lua

--- maincode.lua
+++ maincode.lua
@@ -1,3 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/AEverhub/storehouse219/main/mainsp"))()
-
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AEverhub/storehouse219/main/mainsp", true))()
 --使用此程式造成一切後果自行負責
